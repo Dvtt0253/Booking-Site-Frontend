059ebf23d521bdce185ca1e851b3ff844f3cb3e9
Joie Harvey <Dvtt0253@gmail.com>
2025-05-30 09:25:51 -0500

    Update VerifyMessage.jsx

--- src/VerifyMessage.jsx
+++ src/VerifyMessage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import NewLogo from './NewLogo.jsx';
 function VerifyMessage(){
     return(
-        <div>
+    <>
+   <NewLogo/>
+    <div className="verify-div">
             <h2>Verify Your Email Address</h2>
             <p>You will receive an email shortly to verify your email address. Please verify to proceed.</p>
         </div>
+    </>
+        
     );
 
 }
