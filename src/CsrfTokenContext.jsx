6b5733e639fbd874f6aa238d3a458b103437fb65
Joie Harvey <Dvtt0253@gmail.com>
2025-05-31 12:16:19 -0500

    Update CsrfTokenContext.jsx

--- src/CsrfTokenContext.jsx
+++ src/CsrfTokenContext.jsx
@@ -10,7 +10,7 @@ export function CsrfTokenProvider({children}){
     
 
     useEffect(()=>{
-        const fetchCsrf = setInterval( async () => {
+        export const fetchCsrf = setInterval( async () => {
             try{
                 const response = await fetch('https://booking-site-api.onrender.com/get_user_csrf', {
                     credentials: 'include',
